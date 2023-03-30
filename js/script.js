61f1b4f964065130d2878d708b06315b2f3dfd60
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2023-03-30 19:23:26 +0300

    gallery section

--- js/script.js
+++ js/script.js
@@ -4,9 +4,8 @@ const navbar = document.querySelector("[data-navbar]");
 const navCloseBtn = document.querySelector("[data-nav-close-btn]");
 const navLinks = document.querySelectorAll("[data-nav-link]");
 const header = document.querySelector("[data-header]");
-const searchBtn = document.querySelector("[data-nav-search-btn]");
 
-const navElementArr = [navOpenBtn, navCloseBtn];
+const navElementArr = [navOpenBtn, navCloseBtn, overlay];
 
 const navToggleEvent = (element) => {
   element.forEach((elem) => elem.addEventListener("click", onClick));
@@ -23,11 +22,7 @@ navToggleEvent(navLinks);
 window.addEventListener("scroll", (e) => {
   if (window.scrollY >= 200) {
     header.classList.add("active");
-    navOpenBtn.style.color = "black";
-    searchBtn.style.color = "black";
   } else {
     header.classList.remove("active");
-    navOpenBtn.style.color = "white";
-    searchBtn.style.color = "white";
   }
 });
